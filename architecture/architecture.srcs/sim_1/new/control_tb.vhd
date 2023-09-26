3d09a5c56f08f1de3fbce6d6972023f1a7ef380b
NestorOtzx <nestorortiz017@gmail.com>
2023-09-25 22:40:19 -0500

    control state machine

---
+++ architecture/architecture.srcs/sim_1/new/control_tb.vhd
@@ -0,0 +1,111 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity tb_control is
+end tb_control;
+
+architecture tb of tb_control is
+
+    component control
+        port (clk       : in std_logic;
+              reset     : in std_logic;
+              opcode    : in std_logic_vector (5 downto 0);
+              pc_write  : out std_logic;
+              branch    : out std_logic;
+              iord      : out std_logic;
+              mem_read  : out std_logic;
+              mem_write : out std_logic;
+              ir_write  : out std_logic;
+              reg_dst   : out std_logic_vector (1 downto 0);
+              memtoreg  : out std_logic_vector (1 downto 0);
+              reg_write : out std_logic;
+              alusrca   : out std_logic;
+              alusrcb   : out std_logic_vector (1 downto 0);
+              aluop     : out std_logic_vector (2 downto 0);
+              pcsrc     : out std_logic_vector (1 downto 0));
+    end component;
+
+    signal clk       : std_logic;
+    signal reset     : std_logic;
+    signal opcode    : std_logic_vector (5 downto 0);
+    signal pc_write  : std_logic;
+    signal branch    : std_logic;
+    signal iord      : std_logic;
+    signal mem_read  : std_logic;
+    signal mem_write : std_logic;
+    signal ir_write  : std_logic;
+    signal reg_dst   : std_logic_vector (1 downto 0);
+    signal memtoreg  : std_logic_vector (1 downto 0);
+    signal reg_write : std_logic;
+    signal alusrca   : std_logic;
+    signal alusrcb   : std_logic_vector (1 downto 0);
+    signal aluop     : std_logic_vector (2 downto 0);
+    signal pcsrc     : std_logic_vector (1 downto 0);
+
+    constant TbPeriod : time := 100 ns; -- EDIT Put right period here
+    signal TbClock : std_logic := '0';
+    signal TbSimEnded : std_logic := '0';
+
+begin
+
+    dut : control
+    port map (clk       => clk,
+              reset     => reset,
+              opcode    => opcode,
+              pc_write  => pc_write,
+              branch    => branch,
+              iord      => iord,
+              mem_read  => mem_read,
+              mem_write => mem_write,
+              ir_write  => ir_write,
+              reg_dst   => reg_dst,
+              memtoreg  => memtoreg,
+              reg_write => reg_write,
+              alusrca   => alusrca,
+              alusrcb   => alusrcb,
+              aluop     => aluop,
+              pcsrc     => pcsrc);
+
+    -- Clock generation
+    TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
+
+    -- EDIT: Check that clk is really your main clock signal
+    clk <= TbClock;
+
+    stimuli : process
+    begin
+        -- EDIT Adapt initialization as needed
+        opcode <= (others => '0');
+
+        -- Reset generation
+        -- EDIT: Check that reset is really your reset signal
+        reset <= '1';
+        wait for 100 ns;
+        reset <= '0';
+        wait for 100 ns;
+
+        opcode <= "001000"; --addi
+        
+        wait for 400 ns;
+        
+        opcode <= "001010"; --slti
+        
+        wait for 400 ns;
+        
+        opcode <= "001101"; --ori
+        
+        wait for 400 ns;
+
+        -- Stop the clock and hence terminate the simulation
+        TbSimEnded <= '1';
+        wait;
+    end process;
+
+end tb;
+
+-- Configuration block below is required by some simulators. Usually no need to edit.
+
+configuration cfg_tb_control of tb_control is
+    for tb
+    end for;
+end cfg_tb_control;
---
+++ architecture/architecture.srcs/sources_1/new/control.vhd
@@ -0,0 +1,337 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity control is
+  Port ( 
+        clk : in STD_LOGIC;
+        reset : in STD_LOGIC;
+        opcode: in STD_LOGIC_VECTOR (5 downto 0);
+        
+        pc_write: out STD_LOGIC;
+        branch: out STD_LOGIC;
+        iord: out STD_LOGIC;
+        mem_read: out STD_LOGIC;
+        mem_write: out STD_LOGIC;
+        ir_write: out STD_LOGIC;
+        reg_dst: out STD_LOGIC_VECTOR(1 downto 0);
+        memtoreg: out STD_LOGIC_VECTOR(1 downto 0);
+        reg_write: out STD_LOGIC;
+        alusrca: out STD_LOGIC;
+        alusrcb: out STD_LOGIC_VECTOR (1 downto 0);
+        aluop: out STD_LOGIC_VECTOR (2 downto 0);
+        pcsrc: out STD_LOGIC_VECTOR (1 downto 0)
+   );
+end control;
+
+architecture Behavioral of control is
+
+type control_state is (fetch, decode, write_in_reg --basicos
+                      , beq, j, jal, addi, andi, lui, ori, slti  --inmediatos
+                      , sw, lw, address_computation, write_lw_register--memoria
+                      , r_type, write_in_reg_r
+                      );
+
+signal state : control_state := fetch;
+
+begin
+
+process(clk) is
+begin
+    if(rising_edge(clk)) then
+        if (reset = '1') then
+            state <= fetch;
+        else
+            case state is
+--PRINCIPALES
+                when fetch =>               
+                    pc_write <= '1';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '1';
+                    mem_write <= '0';
+                    ir_write <= '1';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '0';
+                    alusrcb <= "01";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= decode;
+                when decode =>              
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '0';
+                    alusrcb <= "11";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    
+                    if opcode = "001000" then --inmediatos
+                        state <= addi;
+                    elsif opcode = "001100" then
+                        state <= andi;
+                    elsif opcode = "001111" then
+                        state <= lui;
+                    elsif opcode = "001101" then
+                        state <= ori;
+                    elsif opcode = "001010" then
+                        state <= slti;
+                    elsif opcode = "000100" then
+                        state <= beq;
+                    elsif opcode = "000010" then
+                        state <= j;
+                    elsif opcode = "000011" then
+                        state <= jal;
+                    elsif opcode = "101011" then --sw
+                        state <= address_computation;  
+                    elsif opcode = "100011" then --lw
+                        state <= address_computation;  
+                    elsif opcode = "000000" then --TIPO R
+                        state <= r_type;
+                    end if;    
+ --INMEDIATAS
+                when j =>
+                    pc_write <= '1';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '0';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "10";
+                    state <= fetch;
+                when jal =>
+                    pc_write <= '1';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "10";
+                    memtoreg <= "10";
+                    reg_write <= '1';
+                    alusrca <= '0';
+                    alusrcb <= "10";
+                    aluop <= "000";
+                    pcsrc <= "10";
+                    state <= fetch;
+                when beq =>
+                    pc_write <= '0';
+                    branch <= '1';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "00";
+                    aluop <= "001";
+                    pcsrc <= "01";
+                    state <= fetch;
+                when addi =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "10";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= write_in_reg;
+                when andi =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "10";
+                    aluop <= "100";
+                    pcsrc <= "00";
+                    state <= write_in_reg;
+                when lui =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "10";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= write_in_reg;
+                when ori =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "10";
+                    aluop <= "011";
+                    pcsrc <= "00";
+                    state <= write_in_reg;
+                when slti =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "10";
+                    aluop <= "101";
+                    pcsrc <= "00";
+                    state <= write_in_reg;
+                when write_in_reg =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '1';
+                    alusrca <= '0';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= fetch;
+-- MEMORIA , sw, lw, address_computation, write_lw_register
+                 when address_computation =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "10";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    
+                    if (opcode = "101011") then --sw
+                        state <= sw;
+                    else 
+                        state <= lw;
+                    end if;
+                when sw =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '1';
+                    mem_read <= '0';
+                    mem_write <= '1';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '0';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= fetch;
+                when lw =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '1';
+                    mem_read <= '1';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '0';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= write_lw_register;
+                when write_lw_register =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "01";
+                    reg_write <= '1';
+                    alusrca <= '0';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= fetch;
+-- TIPO R , r_type, write_in_reg_r
+                 when r_type =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "01";
+                    memtoreg <= "00";
+                    reg_write <= '0';
+                    alusrca <= '1';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= write_in_reg_r;
+                when write_in_reg_r =>
+                    pc_write <= '0';
+                    branch <= '0';
+                    iord <= '0';
+                    mem_read <= '0';
+                    mem_write <= '0';
+                    ir_write <= '0';
+                    reg_dst <= "00";
+                    memtoreg <= "00";
+                    reg_write <= '1';
+                    alusrca <= '0';
+                    alusrcb <= "00";
+                    aluop <= "000";
+                    pcsrc <= "00";
+                    state <= fetch;      
+             end case;
+        end if;
+    end if;
+end process;
+
+end Behavioral;

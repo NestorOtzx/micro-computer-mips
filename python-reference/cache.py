e363d7c266b241b4f04f164b2976c001ae5d2b4e
Iskandar33 <phanor05@outlook.es>
2023-11-01 17:52:51 -0500

    Avance

--- python-reference/cache.py
+++ python-reference/cache.py
@@ -95,16 +95,16 @@ def getWriteRoad(index, tag):
         arrVias[firstBlock].setDirty(index, 0)
 
         direction = (index * 8) + (tag * 64) #reconstruye la direccion en memoria en base al index y tag
-
+        
         for i in range(8):
-            print("writeback: ", direction+i)
             escribirDisco(direction + i, arrVias[firstBlock].conjuntos[index].data[i])
+        
         writeRoad = getWriteRoad(index, tag)
-        arrVias[writeRoad].setDirty(index, 1)
 
     return writeRoad
 
-def write(dir):
+"""
+def write(dir, dato):
     global arrColas
     queue()
     tag, index, offSet = decoDir(dir)
@@ -117,6 +117,17 @@ def write(dir):
         arrVias[writeRoad].write(tag, index, i, leerDisco(dirIni+i))
     arrVias[writeRoad].setDirty(index, 1)
     arrColas[index].append(writeRoad)
+"""
+def write(dir, dato):
+    global arrColas
+    queue()
+    tag, index, offSet = decoDir(dir)
+    writeRoad = getWriteRoad(index, tag)
+    if (arrVias[writeRoad].getDirty(index) == 0):
+        missW()
+        arrVias[writeRoad].write(tag, index, offSet, dato)
+        arrVias[writeRoad].setDirty(index, 1)
+        arrColas[index].append(writeRoad)
 
 def read(dir):
     global arrColas
@@ -127,18 +138,40 @@ def read(dir):
     flagTag = False
     it = 0
     while not flagTag and it < 4:
-        if arrVias[it].conjuntos[index].tag == tag:
+        if arrVias[it].getDirty(index) == 1 and arrVias[it].conjuntos[index].tag == tag:
             readRoad = it
             flagTag = True
         it += 1
 
-    if flagTag == True and arrVias[readRoad].conjuntos[index].validez == 1:
+    if flagTag == True:
         print(f"Cache | {arrVias[readRoad].conjuntos[index].data[offSet]}")
         hitR() 
     else: #SI NO ESTA EN CACHE, BUSCARLO EN LA MEMORIA
         print("El dato no esta presente en cache")
-        print(f"Memoria | { leerDisco(dir) }")
+        writeRoad = 0
+        flag = False
+        it = 0
+        while not flag and it < 4:
+            if arrVias[it].getDirty(index) == 0:
+                writeRoad = it
+                flag = True
+            it += 1
+        if not flag:
+            writeRoad = arrColas[index].popleft()
+
+        dirIni = dir-offSet
+        for i in range(8):
+            disc = leerDisco(dirIni+i)
+            cache = arrVias[writeRoad].conjuntos[index].data[i]
+            escribirDisco(dirIni + i, cache)
+            arrVias[writeRoad].write(tag, index, i, disc)
+        arrVias[writeRoad].setDirty(index, 1)
+        arrColas[index].append(writeRoad)
+        print(f"Memoria | {leerDisco(dir)}")
         missR()
+        
+
+
 
 def writeMem(dir):
     datoTxt = leerDisco(dir)
@@ -168,7 +201,20 @@ def main():
     global arrVias, arrColas
     resetCache()
     resetMemory()
-    ejemplo1()
+    
+    read(20)
+    read(15)
+    read(45)
+    read(65)
+    printCache()
+    
+    read(22)
+    #write(23, 100)
+    printCache()    
+    read(0)
+    printCache()
+    
+    #ejemplo1()
     printEstadisticas()
 
 """
